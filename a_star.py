478795b8f032753ed8bd4c223216f6846df50bff
Ashton Demmer <a.demmer28@gmail.com>
2023-08-09 19:48:23 -0500

    bulk of code

--- a_star.py
+++ a_star.py
@@ -1,35 +1,81 @@
+class Node():
+    def __init__(self, parent=None, position = None):
+        self.parent = parent
+        self.position = position
+        self.g = 0
+        self.h = 0
+        self.h = 0
+    
+    def __eq__(self, other):
+        return self.position == other.position
 
-endX = 10
-endY = 10
-startNode = (0,0)
-currentNode = startNode
-endNode = (endX, endY)
-openList = [startNode]
-neighborNodes = []
-closedList = []
+def astar(maze, start, end):
+    start_node = Node(None, start)
+    end_node = Node(None, end)
 
-def updatePath():
-    #Increment path by 1 (change color to green)
-    return 0
+    start_node.g = start_node.h = start_node.f = 0
+    end_node.g = end_node.h = end_node.f = 0
 
+    open_list = [start_node]
+    closed_list = []
 
-def getManhattanDistance(x, y):
-    h = (abs(x - endX)) + (abs(y - endY))
-    return h
+    while len(open_list) > 0:
+        print("running")
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+        
+        open_list.pop(current_index)
+        closed_list.append(current_node)
 
-def expandNeighborNodes():
-    #clear openList and replace with neighbors of current node
-    neighborNodes = []
-    neighborNodes.append({'X': })
+        #Reconstructing path
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
 
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
+            #Check bounds
+            if node_position[0] > (len(maze)-1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1])-1) or node_position[1] < 0:
+                continue
 
-def findPath():
-    expandNeighborNodes(currentNode)
-    while currentNode is not endNode:
-        closedList.append(currentNode)
-        expandNeighborNodes(currentNode)
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
 
+            new_node = Node(current_node, node_position)
+            children.append(new_node)
 
+        for child in children:
+            skip_child = False
 
+            for closed_child in closed_list:
+                if child == closed_child:
+                    skip_child = True
+                    break
 
+            if skip_child:
+                continue
+            
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            for open_node in open_list:
+                if child == open_node and child.g >= open_node.g:
+                    skip_child = True
+                    break
+                elif child == open_node:  # Update the g value and parent if we found a shorter path
+                    open_list.remove(open_node)
+                    break
+            
+            if not skip_child:
+                open_list.append(child)
--- main.py
+++ main.py
@@ -1,16 +1,79 @@
 import customtkinter as ctk
+import a_star
+import predefined_setups
+from predefined_setups import course1
 
 root = ctk.CTk()
 root._set_appearance_mode("Dark")
 root.title("Pathfinder")
 
-#Create the main grid for the path
-gridFrame = ctk.CTkFrame(root)
-gridFrame.grid(row=1, column=0)
-for row in range(10):
-    for column in range(20):
-        childFrame = ctk.CTkFrame(gridFrame, fg_color="light blue", width=20, height=20)
-        childFrame.grid(row=row, column=column, padx=0.5, pady=0.5)
+box_size = 80
+canvas = ctk.CTkCanvas(root, width = 2000, height=1200)
+canvas.pack()
 
+#Initial Grid setup
+grid = [[canvas.create_rectangle(x * box_size, y*box_size, (x+1)*box_size, (y+1)*box_size, fill="white")
+         for x in range(25)]
+         for y in range(15)]
+#Define start and end nodes
+canvas.itemconfig(grid[7][0], fill = "green")
+canvas.itemconfig(grid[7][24], fill = "red")
+
+
+
+def findColor(event):
+    global color
+    canvas = event.widget
+    xLocation = event.x // box_size
+    yLocation = event.y // box_size
+    if canvas.itemcget(grid[yLocation][xLocation], "fill") == "white":
+        color = "black" 
+    else:
+        color = "white"
+
+def click(event):
+    canvas = event.widget
+    xLocation = event.x // box_size
+    yLocation = event.y // box_size
+    findColor(event)
+    if canvas.itemcget(grid[yLocation][xLocation], "fill") != "red" and canvas.itemcget(grid[yLocation][xLocation], "fill") != "green":
+        canvas.itemconfig(grid[yLocation][xLocation], fill = color)
+def drag(event):
+    canvas = event.widget
+    xLocation = event.x // box_size
+    yLocation = event.y // box_size
+    if canvas.itemcget(grid[yLocation][xLocation], "fill") != "red" and canvas.itemcget(grid[yLocation][xLocation], "fill") != "green":
+        canvas.itemconfig(grid[yLocation][xLocation], fill = color)
+
+canvas.bind("<Button-1>", click)
+canvas.bind("<B1-Motion>", drag)
+
+
+def createMaze():
+    maze = []
+    for y in range(15):
+        row = []
+        for x in range(25):
+            if canvas.itemcget(grid[y][x], "fill") == "black":
+                row.append(1)
+            else:
+                row.append(0)
+        maze.append(row)
+    return maze
+
+def startPathfinding():
+    start = (7, 0)
+    end = (7, 24)
+    maze = createMaze()
+    # print(maze)
+    path = a_star.astar(maze, start, end)
+    for node in path:
+        x = node[1]
+        y = node[0]
+        if canvas.itemcget(grid[y][x], "fill") != "red" and canvas.itemcget(grid[y][x], "fill") != "green":
+            canvas.itemconfig(grid[y][x], fill = "light blue")
+
+startButton = ctk.CTkButton(root, text="Find Path", command=startPathfinding)
+startButton.pack()
 
 root.mainloop()
